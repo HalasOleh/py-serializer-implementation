08f35ae2fe8e17fc60b9a69a77730e63062d1c78
Oleh Halas <oleggalas8@gmail.com>
2026-05-28 19:37:19 +0300

    Update serializer

--- car/serializers.py
+++ car/serializers.py
@@ -1,7 +1,11 @@
+from django.core.validators import validate_unicode_slug
 from rest_framework import serializers
+
+from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(
@@ -10,3 +14,11 @@ class CarSerializer(serializers.Serializer):
     )
     is_broken = serializers.BooleanField()
     problem_description = serializers.CharField(allow_null=True)
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get("manufacturer", instance.manufacturer)
+        instance.model = validated_data.get("model", instance.model)
+        instance.horse_powers = validated_data.get("horse_powers", instance.horse_powers)
