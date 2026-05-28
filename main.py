da6646c77c310f138b88d8f866e93796f43a253c
Oleh Halas <oleggalas8@gmail.com>
2026-05-28 19:26:47 +0300

    Make migrations

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import io
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
     json_data = JSONRenderer().render(serializer.data)
-    return json_data
+    return json_datas
 
 
 def deserialize_car_object(json: bytes) -> Car:
